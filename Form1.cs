c284201a0f5a86d08da98ee7938258e7b2bdbc3e
Limuncic <145533801+Limuncic@users.noreply.github.com>
2023-11-22 08:37:20 +0100

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pteostaloTekuceVrijeme
+{
+    public partial class Form1 : Form
+    {
+        DateTime pocetno= DateTime.Now;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void toolStripStatusLabel2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            
+            toolStripStatusLabel2.Text = "Trenutno: " + DateTime.Now.ToLongTimeString();
+            TimeSpan proteklo = new TimeSpan();
+            proteklo = DateTime.Now - pocetno;
+            toolStripStatusLabel1.Text = "Proteklo: " +
+            proteklo.Hours.ToString() + ":" + proteklo.Minutes.ToString() + ":" + proteklo.Seconds.ToString();
+            
+        }
+    }
+}
